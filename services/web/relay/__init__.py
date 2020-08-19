3d1efb943287fe031c6bff5d4fad1df089b67ae0
José Valverde <delfimvalverde@gmail.com>
2020-08-19 18:42:57 +0000

    Added Encription, TODO change keys

--- services/web/relay/__init__.py
+++ services/web/relay/__init__.py
@@ -4,6 +4,24 @@ from threading import Thread
 # from relay.Thread_package.thead_classes import *
 # from relay.Arduino import arduino_connection
 # from relay.webRasp_connection.webRasp_connection import WebRaspConnection
+from Crypto.Cipher import AES
+from Crypto.Util import Counter
+
+key = b'Jimmy ffffffffff'
+IV = b'1234567891234567'
+
+def do_encrypt(message):
+    iv_int = int.from_bytes(IV, byteorder='big')
+
+    new_counter = Counter.new(128, initial_value=iv_int)
+    cipher = AES.new(key, AES.MODE_CTR, counter=new_counter)
+    return cipher.encrypt(message)
+
+def do_decrypt(message):
+    iv_int = int.from_bytes(IV, byteorder='big')
+    new_counter = Counter.new(128, initial_value=iv_int)
+    cipher = AES.new(key, AES.MODE_CTR, counter=new_counter)
+    return cipher.decrypt(message)
 
 
 class Web2Relay(Thread):
@@ -23,20 +41,25 @@ class Web2Relay(Thread):
     def run(self):
         while True:
             # waiting for a connection
+            print("accepting")
             self.Web, addr = self.sock.accept()
-            data = self.Web.recv(4096)
+            receveid_data = self.Web.recv(4096)
+            data = do_decrypt(receveid_data)
             if data:
-                data = data.decode().split('_', 1)
-                print("[{}] -> {}".format(self.port, data))
                 try:
-                    print(self.rasps[data[0]])
+                    data = data.decode().split('_')
+                    print("[{}] -> {}".format(self.port, data))
+                    self.Web.sendall(do_encrypt(data[0]))
+                    print("sent")
+                    print(str(receveid_data))
                     # parser.parse(data, self.port, 'client')
                     # if len(parser.Rasp_QUEUE)>0:
                     #    pkt = parser.Rasp_QUEUE.pop()
                     #    #print "got queue Rasp: {}".format(pkt.encode('hex'))
                     #    self.Rasp.sendall(pkt)
                 except Exception as e:
-                    print('client[{}]'.format(self.port), e)
+                    print('client[{}] - {}'.format(self.port, str(e)))
+                    print('client[{}] - {}'.format(self.port, str(receveid_data)))
                 # forward to Rasp
                 # self.Rasp.sendall(data)
 
@@ -55,8 +78,9 @@ class Relay(Thread):
     def __init__(self, from_host, to_host, port):
         super(Relay, self).__init__()
         self.from_host = from_host
-        self.to_host = to_host
+        self.to_host = to_host ##TODO Remove
         self.port = port
+        self.connection_ports = []
         self.running = False
 
     def run(self):
@@ -70,6 +94,4 @@ class Relay(Thread):
 
         self.w2r.start()
         print("[Relay({})] connection established".format(self.port))
-        self.w2r.rasps.update({"palacoulo": "ppcoulo"})
-        self.w2r.rasps.update({"porto": "ppporto"})
         # self.r2r.start()
